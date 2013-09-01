e0944ce57e1415e1ecff2028b24f578cc317c598
Simone Margaritelli <evilsocket@gmail.com>
2013-09-01 15:20:46 +0200

    deprecation fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,7 @@ class SearchController < ApplicationController
     langs  = langs_by_names( tokens ) 
 
     @sources = Rails.cache.fetch "search_page_#{params[:page]}_for_#{tokens.join('_')}", :expires_in => 1.week do
-      Source.search( @phrase, langs ).page( params[:page] ).all
+      Source.search( @phrase, langs ).page( params[:page] ).load
     end
 
     @cloud = @sources.map { |s| s.tags }.flatten.sort { |a,b| a.sources_count <=> b.sources_count }[1..70].shuffle
