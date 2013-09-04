8ac8c9eefede120bac0b6d24c528f24901c254ad
evilsocket <evilsocket@gmail.com>
2013-09-05 01:12:05 +0200

    capified!

---
+++ config/deploy.rb
@@ -0,0 +1,60 @@
+require "bundler/capistrano"
+ 
+server "emoticode.net", :web, :app, :db, primary: true
+ 
+set :application, "emoticode"
+set :user, "evilsocket"
+set :deploy_to, "/var/www/#{application}.net/rails"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+ 
+set :scm, "git"
+set :repository, "git@github.com:evilsocket/#{application}.git"
+set :branch, "master"
+ 
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ 
+after "deploy", "deploy:cleanup"
+ 
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+ 
+
+  secrets = [ 'config/database.yml', 'config/secrets.yml', 'config/development.sphinx.conf', 'config/initializers/omniauth.rb', 'config/initializers/recaptcha.rb' ]
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_ini.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+
+    secrets.each do |secret|
+      put File.read(secret), "#{shared_path}/#{secret}"
+    end
+
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+ 
+  task :symlink_config, roles: :app do
+    secrets.each do |secret|
+      run "ln -nfs #{shared_path}/#{secret} #{release_path}/#{secret}"
+    end
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+ 
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
