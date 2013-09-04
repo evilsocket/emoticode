14afea87e1354187dde40541f63a1da6356df48b
evilsocket <evilsocket@gmail.com>
2013-09-04 14:59:30 +0200

    fixes and sphinx tuning

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,9 @@ class SearchController < ApplicationController
   before_filter :check_query
 
   def search
-    @phrase = params[:what]
-
-    tokens = @phrase.split(/\W/).map { |token| token.downcase.to_sym }
-    langs  = langs_by_names( tokens ) 
-
+    @phrase  = Riddle::Query.escape(params[:what])
     @sources = Source.search( @phrase ).page( params[:page] )
+    
     unless @sources.empty?
       @cloud = @sources.map { |s| s.tags }.flatten.sort { |a,b| a.sources_count <=> b.sources_count }[1..70].shuffle
     else
--- app/indices/source_index.rb
+++ app/indices/source_index.rb
@@ -2,4 +2,6 @@ ThinkingSphinx::Index.define :source, :with => :active_record do
   indexes title, :sortable => true
   indexes description
   indexes text
+
+  where 'private = 0'
 end
