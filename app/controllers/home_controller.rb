f753f1c41b763d8b95dedd0d42685db85b72e702
Simone Margaritelli <evilsocket@gmail.com>
2013-09-19 22:34:38 +0200

    better trending algorithm

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
   def index
-    base  = Source.where( :private => false ).joins(:language,:user => :profile)
-
-    @recent  = base.page( params[:page] )
-    @popular = base.where( 'sources.created_at >= ?', 2.months.ago.to_i ).popular.page( params[:page] )
+    @recent  = Source.where( :private => false ).joins(:language,:user => :profile).page( params[:page] )
+    @popular = Source.where( :private => false ).
+      select('sources.*, ( sources.views / ( ( UNIX_TIMESTAMP() - sources.created_at ) / ( 3600 *24 ) ) ) AS trend').
+      joins(:language,:user => :profile).
+      order('trend desc').
+      where( 'sources.created_at >= ?', 2.months.ago.to_i ).
+      page( params[:page] )
     @cloud   = Rails.cache.fetch '70_shuffled_tags_by_sources_count_and_weight', :expires_in => 1.week do 
       Tag.
         with_sources.
