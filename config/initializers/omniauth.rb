6c168bf73b2b4d4d5327edd15df21cbc45ae9495
evilsocket <evilsocket@gmail.com>
2013-10-04 05:44:24 -0400

    fixed github omniauth roles

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   github = Rails.application.config.secrets['Github']
 
   provider :facebook, facebook['api_key'], facebook['api_secret']
-  provider :github,   github['api_key'],   github['api_secret'], :scope => 'user,user:email'
+  provider :github,   github['api_key'],   github['api_secret'], :scope => 'user:email'
 end
