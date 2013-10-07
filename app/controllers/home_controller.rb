151bb7efb55da14973034500c6abd8c9d088af1c
evilsocket <evilsocket@gmail.com>
2013-10-07 06:29:54 -0400

    more data in homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @recent  = Source.public.page( params[:page] )
-    @popular = Source.public.by_trend.newer_than( 2.months.ago.to_i ).page( params[:page] )
+    pager_params = { :page => params[:page], :per_page => 15 }
+
+    @recent  = Source.public.paginate pager_params
+    @popular = Source.public.by_trend.newer_than( 2.months.ago.to_i ).paginate pager_params
     @cloud   = Tag.cloud.to_a.shuffle!
   end
 end
