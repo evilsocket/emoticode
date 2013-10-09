cb181b8b10c03268bdea93acd70aebe645159536
Simone Margaritelli <evilsocket@gmail.com>
2013-10-10 01:38:15 +0200

    better pagination

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -2,7 +2,7 @@ class LanguageController < ApplicationController
   def archive
     @language = Language.find_by_name params[:name]
     if @language
-      @sources = @language.sources.public.page( params[:page] )
+      @sources = @language.sources.public.paginate( :page => params[:page], :per_page => 16 )
       @podium  = @language.most_active_users
       @cloud   = Tag.cloud(@language).to_a.shuffle!
     else
