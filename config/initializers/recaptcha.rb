bc0aaf7a5d32f568e70c1d260eec7fb88da067fa
Simone Margaritelli <evilsocket@gmail.com>
2013-10-24 23:14:54 +0200

    recaptcha fix

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
   data = Rails.application.config.secrets['Recaptcha']
   
-  config.public_key  = data['public_key']
-  config.private_key = data['private_key']
+  ENV['RECAPTCHA_PUBLIC_KEY']  = config.public_key  = data['public_key']
+  ENV['RECAPTCHA_PRIVATE_KEY'] = config.private_key = data['private_key']
 end
