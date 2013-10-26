6b19a743d0c063b6c2d751eefa269b69d08f5ed1
Simone Margaritelli <evilsocket@gmail.com>
2013-10-26 21:00:41 +0200

    fix

--- lib/facebook_client.rb
+++ lib/facebook_client.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class FacebookClient
   def initialize
     @config = Rails.application.config.secrets['Facebook']
@@ -6,5 +9,13 @@ class FacebookClient
 
   def post( title, url, image = 'http://www.emoticode.net/logo_2.0_200.png' )
     @graph.put_connections @config['page_id'], 'feed', :message => title, :picture => image, :link => url
+  end
+
+  def followers
+    Rails.cache.fetch "FacebookClient#followers", :expire => 60.minutes do
+      data = open("http://api.facebook.com/method/fql.query?format=json&query=select+fan_count+from+page+where+page_id%3D#{@config['page_id']}").read    
+      obj = JSON.parse(data)[0]
+      obj["fan_count"].to_i
+    end
   end
 end
