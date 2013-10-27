f88379ae6abf0249d3935121825fb99ee78a6555
Simone Margaritelli <evilsocket@gmail.com>
2013-10-27 18:28:45 +0100

    fixes

--- app/helpers/blog/posts_helper.rb
+++ app/helpers/blog/posts_helper.rb
@@ -2,13 +2,13 @@ module Blog::PostsHelper
   include SeoHelper
 
   def page_title
-    "Blog - #{@post.category.title} - #{@post.title} | emoticode" rescue super
+    "#{@post.title} | emoticode" rescue super
   end
 
   def metas
     make_seo do |seo|
       unless @post.id.nil?
-        seo.title       = paged "#{@post.category.title} - #{@post.title}"      
+        seo.title       = paged @post.title
         seo.description = paged truncate( @post.description!, :length => 250 )
       end
     end
