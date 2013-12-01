cf75e46777c14316dce874c0a984d5248d2de862
Simone Margaritelli <evilsocket@gmail.com>
2013-12-01 18:22:07 +0100

    fix on api key

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -6,14 +6,14 @@ class Api::V1::ApiController < ActionController::Base
   def restrict_access
     @current_user = nil
 
-    if Rails.env.production?
-      authenticate_or_request_with_http_token do |token, options|
-        @current_user = User.find_by_api_key token      
-      end
-    else
-      @current_user = User.find_by_api_key params[:token]
-      head :unauthorized unless @current_user
-    end
+    # if Rails.env.production?
+    #  authenticate_or_request_with_http_token do |token, options|
+    #    @current_user = User.find_by_api_key token      
+    #  end
+    # else
+    @current_user = User.find_by_api_key params[:token]
+    head :unauthorized unless @current_user
+    # end
 
     @current_user
   end
